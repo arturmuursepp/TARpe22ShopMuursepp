f6b4cb552797c92ac6a0f3b9b530692d749c27dc
Opilane TTHK <opilane@tthk.ee>
2023-10-16 11:38:56 +0300

    added index view with spaceships controller

---
+++ TARpe22ShopMyyrsepp/Controllers/SpaceshipsContoller.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TARpe22ShopMyyrsepp.Data;
+using TARpe22ShopMyyrsepp.Models.SpaceShip;
+
+namespace TARpe22ShopMyyrsepp.Controllers
+{
+    public class SpaceshipsContoller : Controller
+    {
+        private readonly TARpe22ShopMyyrseppContext _context;
+        public SpaceshipsContoller(TARpe22ShopMyyrseppContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var result = _context.Spaceships
+                .OrderByDescending(y => y.CreatedAt)
+                .Select(x => new SpaceshipIndexViewModel
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    PassengerCount = x.PassengerCount,
+                    EnginePower = x.EnginePower
+                });
+            return View(result);
+        }
+    }
+}
